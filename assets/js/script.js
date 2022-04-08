02c946dab20676ee83f04b563c63b648476ad094
Emma Olson <emma.olson794@gmail.com>
2022-04-08 15:28:42 -0500

    plain js get local storage function written

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,51 @@
-// ----------- DECLARE GLOBAL VARIABLES
-var today = dayjs();
+// ----------- DECLARE GLOBAL VARIABLES ---------- //
+
+var today = dayjs(); // get current day
+var thisDay = dayjs(today).format('dddd, MMMM D'); // format current date
+var currentTime = dayjs().hour(); // get current hour
 
 var eventContainer = document.querySelector(".container");
-// var saveBtn = document.getElementsByClassName("saveBtn");
 var currentDayEl = document.querySelector("#currentDay");
 var hourEl = document.getElementsByClassName("hour");
 var textareaEl = document.getElementsByClassName("description");
 var hourElIndex = 0;
-var events = [];
 
-console.log(textareaEl);
+// ---------- DECLARE GLOBAL VARIABLES END ---------- //
 
 // print current date to page
-var thisDay = dayjs(today).format('dddd, MMMM D');
 currentDayEl.textContent = thisDay;
 
-// ---------- EVALUATE HOUR FUNCTION ---------- //
+// ---------- GET EVENT DETAILS FROM LOCAL STORAGE ---------- //
 
-// RUNS ON PAGE LOAD
-// get current time
-var currentTime = dayjs().hour();
+var getEventDetails = function() {
+
+    // select elements with time-block class
+    var timeBlock = document.getElementsByClassName("time-block");
+    console.log(timeBlock);
+    // for each element with a class of time-block
+    for (var b of timeBlock) {
+        console.log(b);
+        // select sibling element of time-block with class="hour" and get data attribute
+        var hour = b.children[0].getAttribute("data-hour");
+        // get from local storage using hourId key
+        var lsData = localStorage.getItem("hour");
+
+        if (lsData) {
+            var eventElement = b.children[1];
+            eventElement.value = lsData;
+            console.log(eventElement.value);
+        }
+        else {
+            return
+        }
+    }
+}
+
+// ---------- EVALUATE HOUR FUNCTION RUNS ON PAGE LOAD ---------- //
 
 var evaluateHour = function() {
     var currentHour = hourEl[hourElIndex];
     var currentHourData = currentHour.getAttribute("data-hour");
-    // console.log(currentHourData);
 
     // grab text area associated with current index
     var currentTextEl = textareaEl[hourElIndex];
@@ -50,77 +71,14 @@ var evaluateHour = function() {
     }
 };
 
-// ---------- EVENT LISTENER FOR SAVE BUTTONS ---------- //
+// ---------- EVENT LISTENER FOR SAVE BUTTON ---------- //
 
-// ON CLICK of Event Container
-// eventContainer.addEventListener("click", function(event) {
-
-//     // check to see if event target has the saveBtn class
-//     var hasClass = event.target.classList.contains("saveBtn");
-//     if (hasClass) {
-//         console.log("click btn");
-//         // save current textarea to local storage
-//     } 
-//     else {
-//         console.log("no click btn");
-//     }
-// });
-
-
-
-// ---------- BLUR EVENT TO CAPTURE USER INPUT ON TEXTAREA ---------- //
-
-var timeBlock = document.getElementsByClassName("time-block");
-var timeBlockIndex = timeBlock[0];
-console.log(timeBlockIndex);
-
-// listen for mouseover on event container element
-eventContainer.addEventListener("mouseenter", function(event) {
-    console.log("mouseenter");
-    
-    // listen for click event on description element
-    timeBlockIndex.addEventListener("click", function(event) {
-        var hasClass = event.target.classList.contains("description");
-
-        // if event target is a description element
-        if (hasClass) {
-            console.log("HAS CLASS");
-            // listen for a blur event to capture value
-            event.target.addEventListener("blur", function(event) {
-                console.log("BLUR EVENT")
-                console.log(event.target.value);
-                // declare variable to hold that value
-                var eventText = event.target.value;
-
-                // listen for click event on button associated button
-                timeBlockIndex.addEventListener("click", function(event) {
-
-                    // if event target is a button element
-                    var hasBtnClass = event.target.classList.contains("saveBtn");
-                    if (hasBtnClass) {
-                        console.log("HAS BTN CLASS")
-                        // save changes to local storage
-                    }
-                    else {
-                        return
-                    }
-                })
-            })
-        } 
-        else {
-            console.log("no class");
-        }    
-    })
-
-});
+// listen for click event on save button
 
 
 evaluateHour();
+getEventDetails();
 
-// WHAT IF INSTEAD OF WRAPPING EACH ROW IN A DIV, THEY'RE WRAPPED IN A FORM
-// AND THE <BUTTON> ELEMENT IS A <SUBMIT> WITH PREVENT DEFAULT
-
-// 
 
 // ---------- INITIAL PSEUDOCODE ---------- //
 
